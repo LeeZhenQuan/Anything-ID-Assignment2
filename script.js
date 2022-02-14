359fc75e31cce6f3d1bbf86127df5c84d0251e91
ShaoYeChng <S10228016@connect.np.edu.sg>
2022-02-15 02:43:55 +0800

    Added Profile Page Added Able to update profile Added Data base for form for buying membership

--- script.js
+++ script.js
@@ -8,14 +8,11 @@ $(document).ready(function () {
     getContacts();
     $("#update-contact-container").hide();
     $("#add-update-msg").hide();
-  
     $("#contact-submit").on("click", function (e) {
       e.preventDefault();
-  
       let name = $("#xName").val();
       let email = $("#xEmail").val();
       let password = $("#xPassword").val();
-  
       let jsondata = {
         "xName": name,
         "xEmail": email,
@@ -25,7 +22,6 @@ $(document).ready(function () {
         "xCard": "Null",
         "xPostalCode": "Null"
       };
-  
       let settings = {
         "async": true,
         "crossDomain": true,
@@ -43,18 +39,14 @@ $(document).ready(function () {
           $("#add-contact-form").trigger("reset");
         }
       }
-  
       $.ajax(settings).done(function (response) {
         console.log(response);
-        
         $("#contact-submit").prop( "disabled", false);
-        
         $("#add-update-msg").show().fadeOut(3000);
-  
         getContacts();
       });
+
     });
-  
     function getContacts(limit = 10, all = true) {
       let settings = {
         "async": true,
@@ -93,14 +85,13 @@ $(document).ready(function () {
       let xPassword = $(this).data("password");
       let xMemberPlan = $(this).data("memberPlan");
       let contactId = $(this).data("id");
-  
+
       $("#update-xName").val(xName);
       $("#update-xEmail").val(xEmail);
       $("#update-xPassword").val(xPassword);
       $("#update-xMemberPlan").val(xMemberPlan);
       $("#update-contact-id").val(contactId);
       $("#update-contact-container").show();
-  
     })
     //login page
     $(".login").submit(function(e){
@@ -108,9 +99,9 @@ $(document).ready(function () {
       validation();
     });
     //update membership
-    $("#purchase").submit(function (e) {
-      console.log("fuck");
+    $("#purchase").submit(async function (e) {
       e.preventDefault();
+      let memberPlan = true;
       let xName = sessionStorage.getItem("Name");
       let xEmail = sessionStorage.getItem("Email");
       let xPassword = sessionStorage.getItem("Password");
@@ -118,21 +109,63 @@ $(document).ready(function () {
       let xPostalCode= $("#postalcode").val();
       let xCard = $("#card").val()
       let contactId = sessionStorage.getItem("id");
-      updateForm(contactId, xName, xEmail, xPassword, xAddress, xPostalCode, xCard);
+      let xMembershipType = $("#MembershipType").val();
+      await updateForm(contactId, xName, xEmail, xPassword, memberPlan, xAddress, xPostalCode, xCard, xMembershipType);
+      location.href = "home2.html";
     });
+    //update profile unlocked
+    $("#unlock-update").click(function(e){
+      e.preventDefault();
+      $("#xName").attr("disabled",false);
+      $("#xEmail").attr("disabled",false);
+      $("#xPassword").attr("disabled",false);
+      $("#xAddress").attr("disabled",false);
+      $("#xPostalCode").attr("disabled",false);
+      $("#xCard").attr("disabled",false);
+    })
+    //loading page will show your profile infor
+    $("#update-form div").ready(function(e){
+      const form = $("#update-form div")
+      form.find("#xName").val(sessionStorage.getItem("Name"));
+      form.find("#xEmail").val(sessionStorage.getItem("Email"));
+      form.find("#xPassword").val(sessionStorage.getItem("Password"));
+      form.find("#xAddress").val(sessionStorage.getItem("Address"));
+      form.find("#xPostalCode").val(sessionStorage.getItem("PostalCode"));
+      form.find("#xCard").val(sessionStorage.getItem("CreditCard"));
+      form.find("#xMemberShipType").val(sessionStorage.getItem("MembershipType"));
+      form.find("#xMembership").val(sessionStorage.getItem("memberPlan"))
+    })
+    $("#update-form").submit(async function(e){
+      e.preventDefault();
+      let xName = sessionStorage.getItem("Name");
+      let xEmail = sessionStorage.getItem("Email");
+      let xPassword = sessionStorage.getItem("Password");
+      let xMemberPlan = sessionStorage.getItem("memberPlan");
+      let xAddress = $("#xAddress").val();
+      let xPostalCode= $("#xPostalCode").val();
+      let xCard = $("#xCard").val()
+      let xMembershipType = sessionStorage.getItem("MembershipType");
+      let contactId = sessionStorage.getItem("id");
+      await updateForm(contactId, xName, xEmail, xPassword, xMemberPlan, xAddress, xPostalCode, xCard, xMembershipType);
+      location.href = "home2.html";
+    })
 //This Put is for changing membership plan to true
-    function updateForm(id,name, email, password, address,postalcode,card) {
-      console.log("fuck");
+    async function updateForm(id,name, email, password,memberPlan, address,postalcode,card,MembershipType) {
       var jsondata = { 
         "xName": name,
         "xPassword": password,
         "xEmail": email,
-        "xMemberPlan": true,
+        "xMemberPlan": memberPlan,
         "xAddress": address,
         "xCard": card, 
-        "xPostalCode": postalcode
+        "xPostalCode": postalcode,
+        "xMembershipType": MembershipType
       };
-       
+      sessionStorage.setItem("memberPlan",memberPlan);
+      sessionStorage.setItem("Address",address);
+      sessionStorage.setItem("PostalCode",postalcode);
+      sessionStorage.setItem("CreditCard",card);
+      sessionStorage.setItem("MembershipType",MembershipType);
       var settings = {
         "async": true,
         "crossDomain": true,
@@ -146,12 +179,8 @@ $(document).ready(function () {
         "processData": false,
         "data": JSON.stringify(jsondata)
       }
-
-      $.ajax(settings).done(function (response) {
+      return $.ajax(settings).done(function (response) {
         console.log(response);
-        
-        $("#update-contact-container").fadeOut(5000);
-        getContacts();
       });
     }
 
@@ -168,7 +197,6 @@ $(document).ready(function () {
           "cache-control": "no-cache"
         },
       }
-  
       $.ajax(settings).done(function (response) {
         console.log(response);
         var username = document.getElementById("UserName").value;
@@ -178,27 +206,31 @@ $(document).ready(function () {
           const element = response[i];
           const xName = element.xName;
           const xPassword = element.xPassword;
-          sessionStorage.setItem("id",element._id);
-          sessionStorage.setItem("Name",element.xName);
-          sessionStorage.setItem("Email",element.xEmail);
-          sessionStorage.setItem("Password",element.xPassword);
           if(username == xName && password == xPassword)
           {
             login = true;
+            sessionStorage.setItem("id",element._id);
+            sessionStorage.setItem("Name",element.xName);
+            sessionStorage.setItem("Email",element.xEmail);
+            sessionStorage.setItem("Password",element.xPassword);
+            sessionStorage.setItem("memberPlan",element.xMemberPlan);
+            sessionStorage.setItem("Address",element.xAddress);
+            sessionStorage.setItem("PostalCode",element.xPostalCode);
+            sessionStorage.setItem("CreditCard",element.xCard);
+            sessionStorage.setItem("MembershipType",element.xMembershipType);
             location.href = "home2.html";
           }
         }
         if (login == false) {
+          alert("Invalid Username Or Password!");
         }
       });
     }
-    
   })
 function myFunction3() {
-  document.getElementById("demo").innerHTML = "Hello Dear Visitor! Thank you for sending the email I wil get back to you shortly </br>"}
-    
-function sendEmail() 
-    {
-        window.location = "mailto:BetaID.NP@gmail.com?subject=Important! From Music website";
-    }
+  document.getElementById("demo").innerHTML = "Hello Dear Visitor! Thank you for sending the email I wil get back to you shortly </br>"
+}
+function sendEmail() {
+  window.location = "mailto:BetaID.NP@gmail.com?subject=Important! From Music website";
+}
   
